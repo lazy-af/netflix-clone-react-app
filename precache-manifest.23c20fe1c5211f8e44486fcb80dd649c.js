59c6b1750cf032569b9299babad2f373bfb9dabf
Shivam <shivamgoyal2822@gmail.com>
2020-08-07 14:28:24 +0530

    Updates

---
+++ precache-manifest.23c20fe1c5211f8e44486fcb80dd649c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76162cc7f51657013695cab63b409e54",
+    "url": "/netflix-clone-react-app/index.html"
+  },
+  {
+    "revision": "cbbd9b2f7fd995da87f1",
+    "url": "/netflix-clone-react-app/static/css/main.90f85946.chunk.css"
+  },
+  {
+    "revision": "cfee1b9053985afd53d4",
+    "url": "/netflix-clone-react-app/static/js/2.f9c0b1be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone-react-app/static/js/2.f9c0b1be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbd9b2f7fd995da87f1",
+    "url": "/netflix-clone-react-app/static/js/main.7fe23919.chunk.js"
+  },
+  {
+    "revision": "3ed795b366a973f5172b",
+    "url": "/netflix-clone-react-app/static/js/runtime-main.80f37c8f.js"
+  }
+]);
